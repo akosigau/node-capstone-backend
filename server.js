72de7b71fea45a77ce9b4ca5979eb7d7041ca57d
akosigau <jamesdabalus25@gmail.com>
2023-02-18 23:46:02 +0800

    updated db port

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ require('./app/routes/auth.routes')(app);
 require('./app/routes/user.routes')(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 'https://sthenos-backend.onrender.com';
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
